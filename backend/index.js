431d3de79b2b39a8a03ac97f62f9b861a34b0734
Le Trung Thanh <tle12340987@gmail.com>
2026-05-27 22:24:20 +0700

    tuàn 2 sử dụng post method và get method

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,38 @@ const PORT = 5000;
 
 // Cho phép React gọi API mà không bị lỗi CORS (sẽ học kỹ ở tuần 2)
 app.use(cors());
+app.use(express.json())
 
 // API Hello World
 app.get('/api/hello', (req, res) => {
   res.send('Hello từ Server');
 });
+app.get('/api/movies', (req, res) => {
+  const movies = [
+    {
+      id: 1,
+      title: "Inception",
+      image: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg"
+    },
+    {
+      id: 2,
+      title: "Interstellar",
+      image: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDItN2IxOS00MTFiLTgwMzYtZTFkMTAxYzViZWIyXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+    },
+    {
+      id: 3,
+      title: "The Dark Knight",
+      image: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg"
+    }
+  ];
 
+  res.json(movies)
+})
+app.post('/api/postMovie', (req, res) => {
+  console.log("dữ liệu nhận được", req.body)
+  console.log("đã nhận được dữ liệu")
+  res.json("đã thêm phim thành công")
+});
 // Chạy server
 app.listen(PORT, () => {
   console.log(`Server đang chạy tại http://localhost:${PORT}`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,58 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
+  const [movies, setmovies] = useState([])
+  const [title, setTitle] = useState('')
+  const [image, setImage] = useState('')
+  useEffect(() => {
+    fetch('http://localhost:5000/api/movies').then(Response => {
+      return Response.json()
+    }).then(data => {
+      setmovies(data)
+    }).catch(error => {
+      console.log("lỗi API", error)
+    })
+  }, [])
+  const handleMovies = (e) => {
+    e.preventDefault()
+    const dataToSend = {
+      title: title,
+      image: image
+    }
+    fetch('http://localhost:5000/api/postMovie', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(dataToSend)
+    }).then(Response => {
+      return Response.json()
+    }).then(data => {
+      console.log("server trả lời", data)
+      alert("thêm phim thành công")
+      setTitle('')
+      setImage('')
+      window.location.reload()
+    })
+      .catch(error => console.log("lỗi API", error))
+  }
   return (
     <div>
-      <h1>Hello từ React</h1>
+      <h1>thông điệp từ backend</h1>
+      <form onSubmit={handleMovies}>
+        <input type="text" placeholder='tên phim' value={title} onChange={((e) => setTitle(e.target.value))} />
+        <input type="text" placeholder='hình ảnh' value={image} onChange={((e) => setImage(e.target.value))} />
+        <button type='submit'>thêm phim</button>
+      </form>
+      <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap' }}>
+        {movies.map((movie) => (
+          <div key={movie.id} style={{ border: '1px solid white', padding: '10px', width: '200px' }}>
+            <h3>{movie.title}</h3>
+            <img src={movie.image} alt={movie.title} style={{ width: '100%', height: 'auto' }} />
+          </div>
+        ))}
+      </div>
     </div>
   )
-}
 
+}
 export default App
