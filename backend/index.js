4837ffec031bfee33773b09837dcb1757591746a
Le Trung Thanh <tle12340987@gmail.com>
2026-06-09 22:34:47 +0700

    thêm packcage của bcrypt jsonwebtoken và bổ sung api đăng ký và đăng nhập

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const app = express();
 const PORT = 5000;
@@ -16,6 +18,66 @@ const Movie = mongoose.model('movie', movieSchema)
 app.use(cors());
 app.use(express.json())
 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+const User = mongoose.model('user', userSchema)
+
+app.post('/api/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const existingUser = await User.findOne({ username: username })
+    if (existingUser) {
+      res.status(400).json('tên đăng nhập đã tồn tại')
+    }
+    const saltRound = 10;
+    const hashPassword = await bcrypt.hash(password, saltRound)
+    const newUser = await
+      User.create({
+        username: username,
+        password: hashPassword
+      })
+    res.json('đăng ký thành công')
+  } catch (error) {
+    res.status(500).json('lỗi khi lấy dữ liệu')
+  }
+})
+
+const JWT_SECRET = 'VURVPcnfhjWFHWOEE121!@$#$'
+app.post('/api/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await user.findOne({ username: username })
+    if (!user) {
+      return res.status(400).json('sai tên đăng nhập')
+    }
+    const isPasswordValid = await bcrypt.compare({ password: user.password })
+    if (!isPasswordValid) {
+      return res.status(400).json('sai mật khẩu')
+    }
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      JWT_SECRET,
+      { expiresIn: '1h' }
+    )
+    res.json({
+      message: "đăng nhập thành công",
+      token: token
+    })
+
+  } catch (error) {
+    res.status(500).json('lỗi khi lấy dữ liệu')
+  }
+
+})
 
 app.get('/api/movies', async (req, res) => {
   try {
